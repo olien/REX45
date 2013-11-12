f1db62c9109b68ce0479f2641744ecac932e5cba
Oliver Kreischer <kreischer@concedra.de>
2013-11-12 16:01:49 +0100

    ff

--- redaxo/include/addons/dev_tools/pages/settings.inc.php
+++ redaxo/include/addons/dev_tools/pages/settings.inc.php
@@ -1,36 +1,3 @@
-<?php
-$page = rex_request('page', 'string');
-$subpage = rex_request('subpage', 'string');
-$func = rex_request('func', 'string');
-$javascriptmethod = rex_request('javascriptmethod', 'string');
-$nojavascriptmethod = rex_request('nojavascriptmethod', 'string');
-
-if (empty($javascriptmethod)) {
-	$javascriptmethod = '0';
-}
-
-if (empty($nojavascriptmethod)) {
-	$nojavascriptmethod = '0';
-}
-
-if ($func == "update") {
-	$REX['ADDON']['email_obfuscator']['javascriptmethod'] = $javascriptmethod;
-	$REX['ADDON']['email_obfuscator']['nojavascriptmethod'] = $nojavascriptmethod;
-	
-$content = '
-$REX[\'ADDON\'][\'email_obfuscator\'][\'javascriptmethod\'] = \''.$javascriptmethod.'\';
-$REX[\'ADDON\'][\'email_obfuscator\'][\'nojavascriptmethod\'] = \''.$nojavascriptmethod.'\';
-$REX[\'ADDON\'][\'email_obfuscator\'][\'noscript_msg\'] = \''.$REX['ADDON']['email_obfuscator']['noscript_msg'].'\';
-$REX[\'ADDON\'][\'email_obfuscator\'][\'noscript_msg_string_table_key\'] = \''.$REX['ADDON']['email_obfuscator']['noscript_msg_string_table_key'].'\';
-';
-
-	$file = $REX['INCLUDE_PATH']."/addons/email_obfuscator/config.inc.php";
-	rex_replace_dynamic_contents($file, $content);
-	
-	echo rex_info('Einstellungen wurde aktualisiert.');
-}
-?>
-
 <div class="rex-addon-output">
 
 <h2 class="rex-hl2">Informationen</h2>
@@ -56,7 +23,6 @@ Anleitung</a>
         <div class="rex-form-row">
 		</div>
         <div class="rex-form-row">
-
 
         <div class="rex-form-row">
         <div class="rex-form-row">
